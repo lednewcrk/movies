f7b87b432739cd698a69dce45422214e77636072
Wendel <rwendelpereira@gmail.com>
2021-08-01 22:42:31 -0300

    🔧 Adding typescript

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Hello from '@components/Hello';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Hello name={'movies'} />
+    </SafeAreaView>
+  );
+};
+
+export default App;
