100d319a1d74d21c4665621671e047e1ca7d9e0a
Wendel <rwendelpereira@gmail.com>
2021-08-05 18:11:37 -0300

    🔇 Remove logs

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -11,14 +11,10 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(config => {
-  console.log('intercept request...');
-
   config.params = Object.assign(config.params ?? {}, {
     api_key: API_KEY,
     language: LANGUAGE,
   });
-
-  console.log(config.params);
 
   return config;
 });
