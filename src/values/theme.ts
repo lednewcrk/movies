4023125241f462fa51aefe0e819d7c476cbb1c1a
Wendel <rwendelpereira@gmail.com>
2021-08-06 16:02:12 -0300

    💄 Add shimmer colors

--- src/values/theme.ts
+++ src/values/theme.ts
@@ -1,6 +1,7 @@
 export const LIGHT_THEME = {
   background: 'white',
   text: 'black',
+  imageShimmer: ['transparent', 'grey', 'transparent'],
 };
 
 export type CustomTheme = typeof LIGHT_THEME;
@@ -8,4 +9,5 @@ export type CustomTheme = typeof LIGHT_THEME;
 export const DARK_THEME: CustomTheme = {
   background: 'black',
   text: 'white',
+  imageShimmer: ['transparent', 'grey', 'transparent'],
 };
