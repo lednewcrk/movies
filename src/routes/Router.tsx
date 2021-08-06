196775277d9ff31a6e8b01da2f3def650842d165
Wendel <rwendelpereira@gmail.com>
2021-08-06 16:01:59 -0300

    ✏️ Declare global rn paper custom theme colors

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
 
-import {CombinedDefaultTheme, CombinedDarkTheme} from './theme';
-
-import RootStack from './root.stack';
 import {AuthenticationProvider} from '@context/authentication/AuthenticationContext';
+import {CustomTheme} from '@values/theme';
+
+import {CombinedDefaultTheme, CombinedDarkTheme} from './theme';
+import RootStack from './root.stack';
+
+declare global {
+  namespace ReactNativePaper {
+    interface ThemeColors extends CustomTheme {}
+
+    interface Theme {
+      myOwnProperty: boolean;
+    }
+  }
+}
 
 export default function Router() {
   const [isDarkTheme, setIsDarkTheme] = React.useState(false);
@@ -19,6 +30,7 @@ export default function Router() {
 
   return (
     <AuthenticationProvider toggleTheme={toggleTheme}>
+      {/* @ts-ignore */}
       <PaperProvider theme={theme}>
         <NavigationContainer theme={theme}>
           <RootStack />
