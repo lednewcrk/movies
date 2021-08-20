de243ce8e087378b37268f07486fdc6327ea6c09
Wendel <rwendelpereira@gmail.com>
2021-08-20 19:03:28 -0300

    🔥 Remove custom image component

--- src/components/Image/types.ts
+++
@@ -1,7 +0,0 @@
-import {ShimmerPlaceholderProps} from 'react-native-shimmer-placeholder';
-import {Source} from 'react-native-fast-image';
-
-export type Props = {
-  source: number | Source;
-  aspectRatio?: number;
-};
